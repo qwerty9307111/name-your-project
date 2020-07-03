23ee908070bcfc3a444aa438a9e05451ff0b169b
yww <2436415068@qq.com>
2020-07-03 16:00:52 +0800

    feat: access Baidu Translation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,13 @@
   <div class="home">
     <!-- <HelloWorld :msg="'Hello World'"></HelloWorld> -->
     <div class="side-bar">
-      <el-button @click="getRandomName" class="submit" type="primary" circle icon="el-icon-arrow-right"></el-button>
+      <el-button
+        @click="getRandomName"
+        class="submit"
+        type="primary"
+        circle
+        icon="el-icon-arrow-right"
+      ></el-button>
       <el-form>
         <el-form-item>
           <el-radio-group class="mt-20" v-model="type">
@@ -10,14 +16,22 @@
           </el-radio-group>
         </el-form-item>
         <el-form-item label="命名规范：">
-          <el-select class="nomenclature" @change="showResult" v-model="nomenclature" placeholder="请选择">
+          <el-select
+            class="nomenclature"
+            @change="showResult"
+            v-model="nomenclature"
+            placeholder="请选择"
+          >
             <el-option
               v-for="item in nomenclatureOptions"
               :key="item.value"
               :label="item.label"
-              :value="item.value">
-            </el-option>
+              :value="item.value"
+            ></el-option>
           </el-select>
+        </el-form-item>
+        <el-form-item v-show="showTextArea">
+          <el-input type="textarea" :rows="2" placeholder="请输入内容" v-model="text"></el-input>
         </el-form-item>
       </el-form>
     </div>
@@ -31,14 +45,24 @@
 import getRandomName from '@/script/getRandomName'
 import changeCase from '@/script/changeCase'
 import cases from '@/script/cases'
+import md5 from 'blueimp-md5'
+import jsonp from 'jsonp'
 
 export default {
   name: 'Home',
   components: {
     // HelloWorld
   },
+  watch: {
+    type () {
+      console.log(this.type)
+      this.showTextArea = this.type === '翻译'
+    }
+  },
   data () {
     return {
+      showTextArea: false,
+      text: '',
       type: '随机',
       types: ['随机', '翻译'],
       nomenclature: '',
@@ -58,9 +82,28 @@ export default {
     })
   },
   methods: {
+    translate (q = '') {
+      const appId = '20200702000511306'
+      const secretKey = 'W3jr9aME239y9PXF0kTu'
+      const salt = (new Date()).getTime()
+      const sign = md5(appId + q + salt + secretKey)
+      const url = `http://api.fanyi.baidu.com/api/trans/vip/translate?q=${encodeURI(q)}&from=zh&to=en&appid=${appId}&salt=${salt}&sign=${sign}&action=1`
+      jsonp(url, null, (err, data) => {
+        console.log('err', err)
+        console.log('data', data)
+        if (data) {
+          this.randomName = data.trans_result[0].dst.split(' ')
+          this.showResult()
+        }
+      })
+    },
     getRandomName () {
-      this.randomName = getRandomName()
-      this.showResult()
+      if (this.type === '随机') {
+        this.randomName = getRandomName()
+        this.showResult()
+      } else {
+        this.translate(this.text)
+      }
     },
     showResult () {
       if (!this.randomName) return false
